cfc4ae3e6ef972afbe8b076056a2010aeed89f83
razvan2122112 <razvanrusu2017@gmail.com>
2025-09-25 14:59:44 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CarAI from './components/CarAI';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CarAI />
+    </div>
+  );
+}
+
+export default App;
